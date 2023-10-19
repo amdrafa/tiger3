67a8bab7f9d051d8c61ff5dce4cdd00b5198ff75
unknown <tmrafinha4@gmail.com>
2023-10-19 15:36:34 -0300

    vsl changed

--- src/pages/creative.tsx
+++ src/pages/creative.tsx
@@ -110,9 +110,9 @@ export function Creative() {
                     <Loading />
                 ) : (
                     <div className="border-[8px] rounded-lg border-red-950 mb-6">
-                        <div dangerouslySetInnerHTML={{ __html: '<div id="vid_64dd65ccfc6f5800092718d8" style="position:relative;width:100%;padding: 178.21782178217822% 0 0;"><img id="thumb_64dd65ccfc6f5800092718d8" src="https://images.converteai.net/5ed6c64f-24d6-4556-9bf4-152ac30aac75/players/64dd65ccfc6f5800092718d8/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"><div id="backdrop_64dd65ccfc6f5800092718d8" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div></div>' }} />
+                        <div dangerouslySetInnerHTML={{ __html: '<div id="vid_65317581e956e500092126d5" style="position:relative;width:100%;padding: 178.21782178217822% 0 0;"><img id="thumb_65317581e956e500092126d5" src="https://images.converteai.net/4bca4dd6-bbad-4057-809c-56b055191e13/players/65317581e956e500092126d5/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"><div id="backdrop_65317581e956e500092126d5" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div></div>' }} />
                         <Helmet>
-                            <script type="text/javascript" id="scr_64dd65ccfc6f5800092718d8">var s=document.createElement("script");s.src="https://scripts.converteai.net/5ed6c64f-24d6-4556-9bf4-152ac30aac75/players/64dd65ccfc6f5800092718d8/player.js",s.async=!0,document.head.appendChild(s);</script>
+                            <script type="text/javascript" id="scr_65317581e956e500092126d5">var s=document.createElement("script");s.src="https://scripts.converteai.net/4bca4dd6-bbad-4057-809c-56b055191e13/players/65317581e956e500092126d5/player.js",s.async=!0,document.head.appendChild(s);</script>
                         </Helmet>
                     </div>
                 )}
